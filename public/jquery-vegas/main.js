ed251cd6d88785e75ff66a32c8d7400065c64b76
mettaworldfit <wjose260@gmail.com>
2025-03-12 15:15:54 -0400

    correccion de errores

--- public/jquery-vegas/main.js
+++ public/jquery-vegas/main.js
@@ -1,10 +1,11 @@
-const URL_ =  window.location.protocol + '//' + window.location.host + '/';
 $(document).ready(function () {
+
+  
   $(".sidebar-right").vegas({
     slides: [
-      { src: URL_ + "public/imagen/img/img1.webp" },
-      { src: URL_ + "public/imagen/img/img2.webp" },
-      { src: URL_ + "public/imagen/img/img3.webp" },
+      { src: window.location.protocol + '//' + window.location.host + '/' + "public/imagen/img/img1.webp" },
+      { src: window.location.protocol + '//' + window.location.host + '/' + "public/imagen/img/img2.webp" },
+      { src: window.location.protocol + '//' + window.location.host + '/' + "public/imagen/img/img3.webp" },
     ],
     transition: ["fade", "blur", "fade2", "blur2"],
   });
