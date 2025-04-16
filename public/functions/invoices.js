b339efdec6fdf6b8f557f4393cbcc58a35836423
mettaworldfit <wjose260@gmail.com>
2025-04-16 09:29:51 -0400

    ajustes

--- public/functions/invoices.js
+++ public/functions/invoices.js
@@ -1474,18 +1474,15 @@ function RegisterDetail(id, onDb = false, data) {
 
                 if (res == "ready") {
 
-                    $('#Detalle').load(location.href + " #Detalle"); // Actualizar detalle
-                    if (onDb == true) {
-
-                        if ($("#sendMail").is(':checked')) {
-                            SendmailQuote(res) // Enviar mail
-                        } else {
-                            GenerateQuotePDF(res) // Generar PDF
-                            console.log('Generando PDF')
-                        }
-
-                        invoice_total() // Cargar total de la cotizacion
+                    if ($("#sendMail").is(':checked')) {
+                        SendmailQuote(res) // Enviar mail
+                    } else {
+                        GenerateQuotePDF(res) // Generar PDF
+                        console.log('Generando PDF')
                     }
+
+                    $('#Detalle').load(location.href + " #Detalle"); // Actualizar detalle
+                    if (onDb == true) return invoice_total() // Cargar total de la cotizacion
                     if (onDb != true) return CancelQuote() // Borrar todo del localstorage
 
                 } else {
