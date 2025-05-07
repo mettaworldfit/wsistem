d0645f0f62f944620380c046c50096186f67dd20
mettaworldfit <wjose260@gmail.com>
2025-05-07 19:17:58 -0400

    buscador nueva consulta agregada

--- services/home.php
+++ services/home.php
@@ -160,18 +160,18 @@ if ($_POST['action'] == 'buscador') {
             ];
         }
 
-        // $factura_rp = $db->query("SELECT f.facturaRP_id,f.orden_rp_id,c.nombre,c.apellidos FROM facturasRP f
-        // INNER JOIN clientes c ON c.cliente_id = f.cliente_id 
-        // WHERE c.nombre LIKE '%$keyword%' or f.facturaRP_id LIKE '%$keyword%' LIMIT 10");
-        // while ($row = $factura_rp->fetch_assoc()) {
-        //     $result[] = [
-        //         'tipo' => 'Factura_reparacion',
-        //         'id' => $row['facturaRP_id'],
-        //         'orden_id' => $row['orden_rp_id'],
-        //         'nombre' => $row['nombre'],
-        //         'apellidos' => $row['apellidos']
-        //     ];
-        // }
+        $factura_rp = $db->query("SELECT f.facturaRP_id,f.orden_rp_id,c.nombre,c.apellidos FROM facturasrp f
+        INNER JOIN clientes c ON c.cliente_id = f.cliente_id 
+        WHERE c.nombre LIKE '%$keyword%' or f.facturaRP_id LIKE '%$keyword%' LIMIT 10");
+        while ($row = $factura_rp->fetch_assoc()) {
+            $result[] = [
+                'tipo' => 'Factura_reparacion',
+                'id' => $row['facturaRP_id'],
+                'orden_id' => $row['orden_rp_id'],
+                'nombre' => $row['nombre'],
+                'apellidos' => $row['apellidos']
+            ];
+        }
     }
 
     echo json_encode($result);
