b9f3758baa0b763b298f219f5db605b99a8cd189
mettaworldfit <wjose260@gmail.com>
2025-12-19 12:53:21 -0400

    probando el pos2

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.0.17');
+define('APP_VERSION', '1.0.18');
 
 // Configuracion del sistema
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
