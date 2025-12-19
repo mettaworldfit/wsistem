acd6087bf71e17f1487b6706432f96daa8515ada
mettaworldfit <wjose260@gmail.com>
2025-12-19 12:12:46 -0400

    probando el pos2

--- public/functions/pos.js
+++ public/functions/pos.js
@@ -49,56 +49,63 @@ $(document).ready(function () {
                 orderDir: 'asc' // Dirección de ordenación ('asc' o 'desc')
             },
             successCallback: (response) => {
+                console.log(response);  // Inspecciona lo que realmente viene en la respuesta
                 try {
-                    const data = JSON.parse(response);
-
-                    // Limpiar el grid antes de agregar los nuevos productos
-                    const gridContainer = $('#product-grid');
-                    gridContainer.empty();
-
-                    if (data.data && data.data.length > 0) {
-                        // Agregar los productos a la cuadrícula
-                        data.data.forEach(product => {
-
-                            // Construir imagen si existe, de lo contrario mostrar ícono SVG
-                            // const productImage = product.imagen && product.imagen !== ""
-                            //     ? `<img src="${SITE_URL}public/imagen/${product.imagen}" 
-                            //                 onerror="this.onerror=null; this.src='${SITE_URL}public/imagen/sistem/no-imagen.png';" 
-                            //                 alt="">`
-                            //                             : `
-                            //         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tags-icon lucide-tags">
-                            //             <path d="M13.172 2a2 2 0 0 1 1.414.586l6.71 6.71a2.4 2.4 0 0 1 0 3.408l-4.592 4.592a2.4 2.4 0 0 1-3.408 0l-6.71-6.71A2 2 0 0 1 6 9.172V3a1 1 0 0 1 1-1z" />
-                            //             <path d="M2 7v6.172a2 2 0 0 0 .586 1.414l6.71 6.71a2.4 2.4 0 0 0 3.191.193" />
-                            //             <circle cx="10.5" cy="6.5" r=".5" fill="currentColor" />
-                            //         </svg>`;
-
-                            const productCard = `
-
-                        <button class="product-card" action="button" data-product="${product.producto_id}" data-desc="${product.nombre_producto}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tags-icon lucide-tags"><path d="M13.172 2a2 2 0 0 1 1.414.586l6.71 6.71a2.4 2.4 0 0 1 0 3.408l-4.592 4.592a2.4 2.4 0 0 1-3.408 0l-6.71-6.71A2 2 0 0 1 6 9.172V3a1 1 0 0 1 1-1z" /><path d="M2 7v6.172a2 2 0 0 0 .586 1.414l6.71 6.71a2.4 2.4 0 0 0 3.191.193" /><circle cx="10.5" cy="6.5" r=".5" fill="currentColor" /></svg>
-                            <div class="product-info">
-                                <p class="pos-stock">inv. ${parseFloat(product.cantidad)}</p>
-                                <span>${product.nombre_producto}</span>
-                                <p class="pos-price">$${product.precio_unitario}</p>
-                            </div>
-                            <input type="hidden" id="price_out" value="${product.precio_unitario}">
-                                <input type="hidden" id="cost" value="${product.precio_costo}">
-                                </button>
-                                `;
-                            gridContainer.append(productCard);
-                        });
-
-                    } else {
-                        console.error("No hay productos disponibles");
-                    }
-
+                    var data = JSON.parse(response);
+                    // Procesa los datos JSON aquí
                 } catch (e) {
-                    console.error("Error al analizar la respuesta JSON del producto:", e);
+                    console.error('Error al analizar la respuesta JSON', e);
                 }
+                // try {
+                //     const data = JSON.parse(response);
+
+                //     // Limpiar el grid antes de agregar los nuevos productos
+                //     const gridContainer = $('#product-grid');
+                //     gridContainer.empty();
+
+                //     if (data.data && data.data.length > 0) {
+                //         // Agregar los productos a la cuadrícula
+                //         data.data.forEach(product => {
+
+                //             // Construir imagen si existe, de lo contrario mostrar ícono SVG
+                //             // const productImage = product.imagen && product.imagen !== ""
+                //             //     ? `<img src="${SITE_URL}public/imagen/${product.imagen}" 
+                //             //                 onerror="this.onerror=null; this.src='${SITE_URL}public/imagen/sistem/no-imagen.png';" 
+                //             //                 alt="">`
+                //             //                             : `
+                //             //         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tags-icon lucide-tags">
+                //             //             <path d="M13.172 2a2 2 0 0 1 1.414.586l6.71 6.71a2.4 2.4 0 0 1 0 3.408l-4.592 4.592a2.4 2.4 0 0 1-3.408 0l-6.71-6.71A2 2 0 0 1 6 9.172V3a1 1 0 0 1 1-1z" />
+                //             //             <path d="M2 7v6.172a2 2 0 0 0 .586 1.414l6.71 6.71a2.4 2.4 0 0 0 3.191.193" />
+                //             //             <circle cx="10.5" cy="6.5" r=".5" fill="currentColor" />
+                //             //         </svg>`;
+
+                //             const productCard = `
+
+                //         <button class="product-card" action="button" data-product="${product.producto_id}" data-desc="${product.nombre_producto}">
+                //             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-tags-icon lucide-tags"><path d="M13.172 2a2 2 0 0 1 1.414.586l6.71 6.71a2.4 2.4 0 0 1 0 3.408l-4.592 4.592a2.4 2.4 0 0 1-3.408 0l-6.71-6.71A2 2 0 0 1 6 9.172V3a1 1 0 0 1 1-1z" /><path d="M2 7v6.172a2 2 0 0 0 .586 1.414l6.71 6.71a2.4 2.4 0 0 0 3.191.193" /><circle cx="10.5" cy="6.5" r=".5" fill="currentColor" /></svg>
+                //             <div class="product-info">
+                //                 <p class="pos-stock">inv. ${parseFloat(product.cantidad)}</p>
+                //                 <span>${product.nombre_producto}</span>
+                //                 <p class="pos-price">$${product.precio_unitario}</p>
+                //             </div>
+                //             <input type="hidden" id="price_out" value="${product.precio_unitario}">
+                //                 <input type="hidden" id="cost" value="${product.precio_costo}">
+                //                 </button>
+                //                 `;
+                //             gridContainer.append(productCard);
+                //         });
+
+                //     } else {
+                //         console.error("No hay productos disponibles");
+                //     }
+
+                // } catch (e) {
+                //     console.error("Error al analizar la respuesta JSON del producto:", e);
+                // }
             },
             errorCallback: (res) => {
                 console.error(res);
-            },verbose: true
+            }, verbose: true
         })
     }
 
