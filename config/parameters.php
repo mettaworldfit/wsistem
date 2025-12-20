041da5f995abeade60a6b92ab02cb7fe7020db03
mettaworldfit <wjose260@gmail.com>
2025-12-20 18:07:56 -0400

    se han agregado imagenes a los productos

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.0.8');
+define('APP_VERSION', '1.1.0');
 
 // Configuracion del sistema
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
