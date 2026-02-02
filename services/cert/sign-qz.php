b8e227a3a38c5c49367602529724d27ed598d1b6
mettaworldfit <wjose260@gmail.com>
2026-02-01 22:56:17 -0400

    test

--- services/cert/sign-qz.php
+++ services/cert/sign-qz.php
@@ -2,25 +2,23 @@
 
 // Sample key.  Replace with one used for CSR generation
 // $KEY = 'C:/wamp64/www/proyecto/services/cert/private-key.pem';
-$KEY =  $_SERVER['DOCUMENT_ROOT'] . "/services/cert/private-key.pem";
 
-//$PASS = 'S3cur3P@ssw0rd';
+
+// Ruta relativa
+$KEY = $_SERVER['DOCUMENT_ROOT'] . "/wsistem/services/cert/private-key.pem";
+
+// Verifica si el archivo existe
+if (file_exists($path)) {
+    echo "La ruta existe: " . $path;
+} else {
+    echo "La ruta no existe.";
+}
 
 $req = $_GET['request'];
 $privateKey = openssl_get_privatekey(file_get_contents($KEY) /*, $PASS */);
 
 $signature = null;
 openssl_sign($req, $signature, $privateKey, "sha512"); // Use "sha1" for QZ Tray 2.0 and older
-
-/*
-// Or alternately, via phpseclib
-include('Crypt/RSA.php');
-$rsa = new Crypt_RSA();
-$rsa.setHash('sha512'); // Use 'sha1' for QZ Tray 2.0 and older
-$rsa->loadKey(file_get_contents($KEY));
-$rsa->setSignatureMode(CRYPT_RSA_SIGNATURE_PKCS1);
-$signature = $rsa->sign($req);
-*/
 
 if ($signature) {
 	header("Content-type: text/plain");
