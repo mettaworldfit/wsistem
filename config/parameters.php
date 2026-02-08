066dab07c176cf7a4e87039c4bfa59ae4ca62605
mettaworldfit <wjose260@gmail.com>
2026-02-07 20:28:11 -0400

    Dejar de rastrear qz-certificate.txt

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define('APP_VERSION', '1.3.31');
+define('APP_VERSION', '1.3.32');
 
 // Configuracion del sistema
 $protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https://' : 'http://';
