e7e9fe85d4a7474beb2de1b6d9097658484dc0df
mettaworldfit <wjose260@gmail.com>
2026-06-11 13:02:36 -0400

    Solucionando error

--- src/modules/invoices/invoices.services.js
+++ src/modules/invoices/invoices.services.js
@@ -1,7 +1,7 @@
 // import * as qz from "./src/services/printing/qz/connection.js";
 // import * as printer from "./src/services/printing/templates/invoice.js";
 
-import * as qz from "../../services/printing/qz/connection.js";
+import * as qz from "../../../public/qz/connection.js";
 import * as printer from "../../services/printing/templates/invoice.js";
 import { calculateTotalInvoice, cashBack } from "../../functions/functions.js";
 
